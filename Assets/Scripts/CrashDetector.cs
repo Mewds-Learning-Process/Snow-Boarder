634d5664701eda157aeb40476350772bd470dd04
Mohamed ElHariri <mohamed-elhariri@hotmail.com>
2023-10-31 20:08:03 +0200

    Added code to make the game restart
    To the crash detector and finishline scripts.

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class CrashDetector : MonoBehaviour
 {
@@ -8,7 +10,7 @@ public class CrashDetector : MonoBehaviour
     {
         if(other.tag == "Ground")
         {
-            Debug.Log("you BONKED!!!");
+            SceneManager.LoadScene(0);
         }
         
     }
